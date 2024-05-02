693586ef53cfabacc17702ff9805961ec27c81d0
azanra <112457939+azanra@users.noreply.github.com>
2024-05-02 15:39:39 +0700

    Add function to split, reverse, and join a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(givenString) {
+    let splittedString = givenString.split('');
+    let reversedString = splittedString.reverse();
+    let joinString = reversedString.join('')
+    return joinString;
 };
 
 // Do not edit below this line
