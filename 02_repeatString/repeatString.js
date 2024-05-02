ab571a6ffff535e90dbf8ea20eac228c3b23ef35
azanra <112457939+azanra@users.noreply.github.com>
2024-05-02 14:24:58 +0700

    Add function to multiply string to given number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, amount) {
+    let tempString = '';
+    if(amount < 0){
+        return "ERROR";
+    }
+    else{
+        for(let i = 0; i < amount; i++){
+            tempString += givenString;
+        }
+        return tempString;
+    }
 };
 
 // Do not edit below this line
