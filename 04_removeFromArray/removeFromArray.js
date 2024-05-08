688651bfee0ad4447cbecf73d88e7df40deb3bb3
azanra <112457939+azanra@users.noreply.github.com>
2024-05-08 09:00:10 +0700

    Add function to delete argument from given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray,...toDelete) {
+    let newArray = [];
+    newArray = givenArray.filter((item) => !toDelete.includes(item));
+    return newArray;
 };
 
 // Do not edit below this line
