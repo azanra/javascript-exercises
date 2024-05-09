f4b23606645fdd5587281f1c2d349586e38d68df
azanra <112457939+azanra@users.noreply.github.com>
2024-05-09 13:23:14 +0700

    Add function to add in between min to max number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    let result = 0;
+    let isPositive = Math.sign(startNumber);
+    let isNumber = Number.isInteger(endNumber);
+    if(isPositive === 1 && isNumber === true){
+        if(startNumber < endNumber ){
+            for(let i = startNumber; i <= endNumber; i++){
+                result += i;
+            }
+        }
+        else{
+            for(let i = startNumber; i >= endNumber; i--){
+                result += i;
+            }
+        }
+        return result; 
+    }
+    else{
+        return 'ERROR';
+    }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
