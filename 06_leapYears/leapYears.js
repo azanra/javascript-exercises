660f716a0d68be10527442b3e39cfa906c182b59
azanra <112457939+azanra@users.noreply.github.com>
2024-05-11 13:48:55 +0700

    Add function to determine a leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if(yearInput % 400 === 0 || yearInput % 4 === 0 && yearInput % 100 !== 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
