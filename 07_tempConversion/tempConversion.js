851765f5c6e6b18408219cd91bfd67455bf6713a
azanra <112457939+azanra@users.noreply.github.com>
2024-05-12 09:29:43 +0700

    Add function to convert celcius - fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheit) {
+  let celcius = 0;
+  celcius = (fahrenheit - 32) * 5 / 9;
+  var rounded = Math.round(celcius * 10) / 10;
+  return rounded;
+};  
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = 0;
+  fahrenheit = (celcius * 9 / 5 + 32);
+  var rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
