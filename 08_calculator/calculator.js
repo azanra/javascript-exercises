77ad423ea7ad5096dc82135bfb4ed4101017a02c
azanra <112457939+azanra@users.noreply.github.com>
2024-08-09 12:57:12 +0700

    Add function to calculate each operator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayInput) {
+	return arrayInput.reduce((total, current) => total += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayInput) {
+  return arrayInput.reduce((total, current) => total *= current, );
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = 1;
+  for(let i = 0 ; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let total = 1;
+  if(input === 0){
+    total = 1;
+  }
+  // for(let i = 1; i <= input; i++){
+  //    total *= i;
+  // }
+
+  for(let i = input; i > 0; i--){
+    total *= i;
+  }
+  return total; 
 };
 
 // Do not edit below this line
