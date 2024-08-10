4eaa8749394f4922ae008deac974175769e25bc0
azanra <112457939+azanra@users.noreply.github.com>
2024-08-10 08:34:46 +0700

    Add function to check whether str is palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+    let caseInsensitive = inputStr.toLowerCase();
+    let splitStr = caseInsensitive.split("");
+    for(let i = 0; i < splitStr.length; i++){
+        if(splitStr[i] === " " || splitStr[i] === "!" || splitStr[i] === "," || splitStr[i] === "." ) {
+            splitStr.splice(i, 1);
+            i--;
+        }
+    }
+    let inputStrCopy = splitStr.join("");
+    let reverseStr = splitStr.reverse();
+    let joinStr = reverseStr.join("");
+    if(inputStrCopy === joinStr) {
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
 
