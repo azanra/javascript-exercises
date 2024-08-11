5f131f114973f90c16a6dc3fcdd321be7b843b2a
azanra <112457939+azanra@users.noreply.github.com>
2024-08-11 10:15:55 +0700

    Add function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayObj) {
+     arrayObj.map((element) => {
+        if(element.yearOfDeath === undefined){
+            let currentDate = new Date();
+            element.yearOfDeath =  currentDate.getFullYear();
+        }
+        element.age = element.yearOfDeath - element.yearOfBirth;
+    });
 
+    arrayObj.sort((a, b) => b.age - a.age);
+
+    return arrayObj[0];
 };
 
 // Do not edit below this line
