221aff1ead1723dd60d23b25c59a3a5cc629bcaa
azanra <112457939+azanra@users.noreply.github.com>
2024-08-11 08:20:24 +0700

    Add function to return fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    input = Number(input);
+    let fibonacciArr = [1];
+    let total, previous, current;
+    if(input === 0){
+        return 0;
+    }
+    else if(input < 0){
+        return 'OOPS';
+    }
+
+    for(let i = 0; i < input; i++){
+        if(i === 0){
+            previous = 0;
+            current = 1;
+        }
+        total = previous + current;
+        previous = current;
+        current = total;
+        fibonacciArr.push(total);
+    }
+    console.log(fibonacciArr);
+    return fibonacciArr[input - 1];
 
 };
 
